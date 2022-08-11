b0ed5d0fe46ab9a0e8f60c17b025b15e9a13ef7a
Gustavo Kendy de Souza Kishima <gustavo.kishima@brasilparalelo.com.br>
2022-08-11 16:28:32 -0300

    First commit. My dotfiles has been born.

---
+++ .emacs.d/init.el
@@ -0,0 +1,40 @@
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+
+(package-initialize)
+(package-install 'poet-theme)
+(if (display-graphic-p) (load-theme 'poet t))
+(unless (package-installed-p 'clojure-mode)
+  (package-install 'clojure-mode))
+(unless (package-installed-p 'cider)
+  (package-install 'cider))
+(unless (package-installed-p 'paredit)
+  (package-install 'paredit))
+(unless (package-installed-p 'magit)
+  (package-install 'magit))
+(toggle-frame-fullscreen)
+(add-hook 'cider-repl-mode-hook #'paredit-mode)
+(add-hook 'cider-mode-hook #'paredit-mode)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2d035eb93f92384d11f18ed00930e5cc9964281915689fa035719cab71766a15" default))
+ '(initial-frame-alist '((fullscreen . maximized)))
+ '(package-selected-packages '(paredit clojure-mode)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ README.md
@@ -0,0 +1,3 @@
+# dotfiles
+
+My personal dotfiles config. Right now, it only contains my vanilla Emacs setup with clojure, paredit, magit and a cool theme
