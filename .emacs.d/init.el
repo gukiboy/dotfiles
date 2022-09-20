e50d5fad33d46f2d894a62824f3d861a5f92de51
Gustavo Kendy de Souza Kishima <gustavo.kishima@brasilparalelo.com.br>
2022-09-19 21:55:23 -0300

    defflow to cider

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,6 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-test-defining-forms '("deftest" "defspec" "defflow"))
  '(custom-safe-themes
    '("2d035eb93f92384d11f18ed00930e5cc9964281915689fa035719cab71766a15" default))
  '(initial-frame-alist '((fullscreen . maximized)))
@@ -38,3 +39,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
